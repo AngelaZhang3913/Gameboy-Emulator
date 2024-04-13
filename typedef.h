5cb01a0892cbca7c1e7798d8c72bc919b0147c4e
Angela Zhang <angelazhang3913@gmail.com>
2024-04-13 17:29:35 -0500

    comments in typedef

--- typedef.h
+++ typedef.h
@@ -1,4 +1,4 @@
-typedef unsigned char BYTE ;
+typedef unsigned char BYTE ; // 8 bits
 typedef char SIGNED_BYTE ;
-typedef unsigned short WORD ;
+typedef unsigned short WORD ; // 16 bits
 typedef signed short SIGNED_WORD ;
