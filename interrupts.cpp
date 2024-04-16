04843f18037ac93e1d676aaee482557f16241b14
Angela Zhang <angelazhang3913@gmail.com>
2024-04-15 20:55:21 -0500

    fixed graphics errors

--- interrupts.cpp
+++ interrupts.cpp
@@ -1,5 +1,10 @@
 #include "interrupts.h"
+#include "hardware.h"
 
 void request_interrupts(int id) {
+    BYTE request = read_memory(0xFF0F);
+}
 
+BYTE bitset(BYTE byte, int bit) {
+    return byte + 1 >> bit;
 }
