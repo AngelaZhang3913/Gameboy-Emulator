3ada8c5afc7bc614511b9e0a72a35dedabb3e9c7
anya-goyal <anyagrocks@gmail.com>
2024-04-17 12:06:10 -0500

    worked on do_interrupts and service_interrupt

--- interrupts.cpp
+++ interrupts.cpp
@@ -7,3 +7,31 @@ void request_interrupt(int id) {
     request = bitset(request, id);
 
 }
+
+void do_interrupts() {
+    if (interrupt_switch) {
+        BYTE request = read_memory(0xFF0F);
+        BYTE enabled = read_memory(0xFFFF);
+        // checks for any pending interrupts
+        if (request > 0) {
+            // checks interrupts in their priority order
+            for (int i = 0; i < 5; i++) {
+                if (test_bit(request,i) && test_bit(enabled, i)) {
+                    // executes the interrupt
+                    sevice_interrupt(i);
+                }
+            }
+        }
+    }
+}
+
+void sevice_interrupt(int id) {
+    interrupt_switch = false;
+    BYTE request = read_memory(0xFF0F);
+    request = bitset(request, id);
+    write_memory(0xFF0F, request);
+
+    // push PC onto stack to save execution address
+    // based on PC's current value, can determine what kind
+    // of interrupt service routine it is
+}
--- interrupts.h
+++ interrupts.h
@@ -1,3 +1,9 @@
 #include "typedef.h"
 
 extern void request_interrupt(int id) ;
+
+extern void do_interrupts() ;
+
+extern void sevice_interrupt(int id) ;
+
+bool interrupt_switch;
