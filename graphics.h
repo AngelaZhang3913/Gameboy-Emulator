c0c85cddb29cfe53aa0f6bd3f233518aadb3e9b8
Angela Zhang <angelazhang3913@gmail.com>
2024-04-20 13:30:06 -0500

    made graphics window

---
+++ graphics.h
@@ -0,0 +1,4 @@
+#include "typedef.h"
+
+extern BYTE screen_data[160][144][3] ;
+extern void update_graphics(int cycles) ;
--- main.cpp
+++ main.cpp
@@ -1,11 +1,65 @@
 #include "hardware.h"
 #include "load_game.h"
 #include "emulator.h"
+#include<unistd.h>
+#include <glut/glut.h>
+#include "graphics.h"
+
+int WIDTH = 160;
+int HEIGHT = 144;
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glDrawPixels(WIDTH, HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, screen_data);
+    glutSwapBuffers(); 
+}
 
 int main(int argc, char* argv[]) {
     load_game_method(); // possibly use args to determine which game to load
-    while (true) {
+
+    // set up glut window
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+    glutInitWindowSize(WIDTH, HEIGHT); 
+    glutInitWindowPosition(100, 100); 
+
+    int MainWindow = glutCreateWindow("Hello Graphics!!"); 
+    glClearColor(0.0, 0.0, 0.0, 0);
+
+    while (true) { // call the update method 60 times in a second
         update();
-        // sleep
+        glutDisplayFunc(display);
+        sleep(0.016);
     }
 }
+
+
+
+/*void makePixel(int x, int y, int r, int g, int b, GLubyte* pixels, int width, int height)
+{
+    if (0 <= x && x < width && 0 <= y && y < height) {
+        int position = (x + y * width) * 3;
+        pixels[position] = r;
+        pixels[position + 1] = g;
+        pixels[position + 2] = b;
+    }
+}*/
+
+/*int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+    glutInitWindowSize(WIDTH, HEIGHT); 
+    glutInitWindowPosition(100, 100); 
+
+    int MainWindow = glutCreateWindow("Hello Graphics!!"); 
+    glClearColor(0.0, 0.0, 0.0, 0);
+
+    //makePixel(200,200,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    glutDisplayFunc(display); 
+    glutMainLoop();
+    return 0;
+}*/
