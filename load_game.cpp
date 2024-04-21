47a673cbc6bc99f056c7e661f40442e86277f45c
shrutinarwaney <shruti.narwaney@gmail.com>
2024-04-21 11:25:31 -0500

    Added test game (super mario)

--- load_game.cpp
+++ load_game.cpp
@@ -10,7 +10,7 @@ void load_game_method() {
     memset(cartridge_memory, 0, sizeof(BYTE)) ;
 
     FILE *game_in;
-    game_in = fopen( "Mario.gb", "rb" );
+    game_in = fopen( "Super_Mario_Land_4.gb", "rb" );
     fread(cartridge_memory, 1, 0x200000, game_in);
     fclose(game_in);
 }
